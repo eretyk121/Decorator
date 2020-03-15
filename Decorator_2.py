7227370ea27957e938226096fa44a16caca7114f
eretyk121 <58330074+eretyk121@users.noreply.github.com>
2020-03-15 13:50:20 +0300

    Add files via upload

---
+++ Decorator_2.py
@@ -0,0 +1,30 @@
+import hashlib
+import datetime
+
+def parametrized_path(path_to_logs):
+    def logger(foo):
+        def new_foo(*args):
+            start_time = str(datetime.datetime.now())
+            name_func = foo.__name__
+            result = foo(*args)
+            with open(path_to_logs, 'w', encoding='utf-8') as text:
+                text.write(f'Начало работы функции {name_func} с аргументами {args}: {start_time}\n')
+                text.write(f'Результат работы функции:\n')
+                for one_line in result:
+                    text.writelines(f'{one_line}\n')
+            return result
+        return new_foo
+    return logger
+
+logger = parametrized_path('result_2.txt')
+@logger
+def hash_text(path):
+    with open(path, encoding='utf-8') as text:
+        for line in text:
+            md5_line = hashlib.md5(line.encode())
+            yield md5_line.hexdigest()
+
+if __name__ == '__main__':
+
+    hash_text('DE.txt')
+
